3127543993d463192a5cbd16001b9e13d84355d4
이준성 <@14.35.227.110>
2023-01-18 03:13:53 +0900

    게임 만들기 1/17일 미완성

---
+++ src/com/Game/Character.java
@@ -0,0 +1,27 @@
+package com.Game;
+
+public class Character  extends DTO {
+
+	public Character() {
+		
+		setExp(0);
+		set
+		public class Yongmin extends DTO {
+		    public Yongmin() {
+		        setName("김용민");
+		        setExp(0);
+		        setAge(24);
+		    }
+		    public void checkyoungmin() {
+
+
+
+}
+}
+
+
+
+
+
+
+
---
+++ src/com/Game/DTO.java
@@ -0,0 +1,108 @@
+package com.Game;
+
+public class DTO {
+
+	
+	private int num;
+	private int exp;
+	private String job;
+	private int gold;
+	private String dungeon;
+	private String monster;
+	private int upgrade;
+	
+	
+	public DTO(int num, int exp, String job, int gold, String dungeon, String monster, int upgrade) {
+		super();
+		this.num = num;
+		this.exp = exp;
+		this.job = job;
+		this.gold = gold;
+		this.dungeon = dungeon;
+		this.monster = monster;
+		this.upgrade = upgrade;
+	}
+
+
+	public int getNum() {
+		return num;
+	}
+
+
+	public void setNum(int num) {
+		this.num = num;
+	}
+
+
+	public int getExp() {
+		return exp;
+	}
+
+
+	public void setExp(int exp) {
+		this.exp = exp;
+	}
+
+
+	public String getJob() {
+		return job;
+	}
+
+
+	public void setJob(String job) {
+		this.job = job;
+	}
+
+
+	public int getGold() {
+		return gold;
+	}
+
+
+	public void setGold(int gold) {
+		this.gold = gold;
+	}
+
+
+	public String getDungeon() {
+		return dungeon;
+	}
+
+
+	public void setDungeon(String dungeon) {
+		this.dungeon = dungeon;
+	}
+
+
+	public String getMonster() {
+		return monster;
+	}
+
+
+	public void setMonster(String monster) {
+		this.monster = monster;
+	}
+
+
+	public int getUpgrade() {
+		return upgrade;
+	}
+
+
+	public void setUpgrade(int upgrade) {
+		this.upgrade = upgrade;
+	}
+	
+	public String getInformation() {
+		return "MemberDTO [ "+
+				" num = " + num +
+				", exp=" + exp +
+				", gold=" + gold +
+				", dungeon=" + dungeon +
+				", monster= " + monster +
+				", job = " + job +
+				", upgrade = " + upgrade +
+				"]";
+	
+}
+}
